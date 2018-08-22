7934a19889b7cbb7143d480d0fdfcf0d81066fdd
csarov-sf <csarov@salesforce.com>
2018-08-22 13:56:51 +0300

    Finished SentEvent tests

---
+++ test/SentEvent.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const {clientId, clientSecret, origin} = require('./test.config');
+const ET_Client = require('../lib/ET_Client');
+
+
+describe('SentEvent', function () {
+
+    this.timeout(5000);
+    let client;
+
+    before(() => {
+        client = new ET_Client(clientId, clientSecret, origin);
+    });
+
+    describe('Get', () => {
+        it('should return an object with items', done => {
+            const props = ['SendID'];
+            client.sentEvent({props}).get((err, response) => {
+                if (err) throw new Error(err);
+                assert.equal(response.res.statusCode, 200);
+                assert(Array.isArray(response.body.Results));
+                done();
+            });
+        });
+    });
+
+});
