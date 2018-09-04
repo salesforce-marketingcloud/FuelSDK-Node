a440d11886c6dd849cd70939023e1dff9f620a8c
csarov-sf <csarov@salesforce.com>
2018-09-04 16:23:59 +0300

    some proof of concept for breaking changes improvements

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -13,77 +13,68 @@ describe('Campaign', function () {
     });
 
     describe('List', () => {
-        it('should return an object with items', done => {
-            client.campaign().list((err, response) => {
-                if (err) throw new Error(err);
-                assert.equal(response.res.statusCode, 200);
-                assert(Array.isArray(response.body.items));
-                done();
-            });
+        it('should return an object with items', async () => {
+            const {items} = await client.campaign.list();
+            assert(Array.isArray(items));
         });
     });
 
-    describe('Post', () => {
-        it('should create', done => {
-            const props = {
-                name: 'Some test campaign name',
-                description: 'Campaign description',
-            };
-            client.campaign({props}).post((err, response) => {
-                if (err) throw new Error(err);
-                assert.equal(response.res.statusCode, 200);
-                assert.equal(response.body.name, props.name);
-                assert.equal(response.body.description, props.description);
-                createdCampaignId = response.body.id;
-                done();
-            });
+    describe('Create', () => {
+        const data = {
+            name: 'Some test campaign name',
+            description: 'Campaign description',
+        };
+        it('should fail if no props passed', async () => {
+            await assert.rejects(() => client.campaign.create());
+        });
+        it('should create', async () => {
+            const body = await client.campaign.create(data);
+            assert.equal(body.name, data.name);
+            assert.equal(body.description, data.description);
+            createdCampaignId = body.id;
         });
     });
 
     describe('Get', () => {
-        it('should fail if no props passed', done => {
-            client.campaign().get(err => {
-                assert(err);
-                done();
-            });
+        it('should fail if no props passed', async () => {
+            await assert.rejects(() => client.campaign.get());
         });
-        it('should get it if createdCampaignId is passed', done => {
-            client.campaign({id: createdCampaignId}).get((err, response) => {
-                if (err) throw new Error(err);
-                assert.equal(response.res.statusCode, 200);
-                assert.equal(response.body.id, createdCampaignId);
-                done();
-            });
+        it('should error 404 if random id is passed', async () => {
+            await assert.rejects(() => client.campaign.get(1234567890));
         });
-        it('should error 404 if random id is passed', done => {
-            client.campaign({id: 1234567890}).get((err, response) => {
-                if (err) throw new Error(err);
-                assert.equal(response.res.statusCode, 400); // api is wrong
-                done();
-            });
+        it('should get it if createdCampaignId is passed', async () => {
+            const body = await client.campaign.get(createdCampaignId);
+            assert.equal(body.id, createdCampaignId);
+        });
+    });
+
+    describe('Update', () => {
+        const newData = {
+            name: 'Modified campaign name',
+            description: 'Modified campaign description',
+        };
+        it('should fail if no props passed', async () => {
+            await assert.rejects(() => client.campaign.update());
+        });
+        it('should error 404 if random id is passed', async () => {
+            await assert.rejects(() => client.campaign.update(1234567890, newData));
+        });
+        it('should update it if createdCampaignId is passed', async () => {
+            const body = await client.campaign.update(createdCampaignId, newData);
+            assert.equal(body.name, newData.name);
+            assert.equal(body.description, newData.description);
         });
     });
 
     describe('Delete', () => {
-        it('should fail if no props passed', done => {
-            client.campaign().delete(err => {
-                assert(err);
-                done();
-            });
+        it('should fail if no props passed', async () => {
+            await assert.rejects(() => client.campaign.delete());
         });
-        it('should delete it if createdCampaignId is passed', done => {
-            client.campaign({id: createdCampaignId}).delete(err => {
-                if (err) throw new Error(err);
-                assert(!err);
-                done();
-            });
+        it('should error 404 if random id is passed', async () => {
+            await assert.rejects(() => client.campaign.delete(1234567890));
         });
-        it('should error 404 if random id is passed', done => {
-            client.campaign({id: 1234567890}).delete((err, response) => {
-                if (err) throw new Error(err);
-                assert.equal(response.res.statusCode, 400); // api is wrong
-                done();
-            });
+        it('should delete it if createdCampaignId is passed', async () => {
+            await assert.doesNotReject(() => client.campaign.delete(createdCampaignId));
         });
     });
 
