16c16998ecb01eaa69d4871c763b7ee6c242300a
csarov-sf <csarov@salesforce.com>
2018-10-04 11:02:33 +0300

    3. If SOAP endpoint is not provided and if /platform/endpoint call fails, we need to take the default SOAP endpoint.

--- test/requests.test.js
+++ test/requests.test.js
@@ -6,7 +6,10 @@ const ET_Client = require('../lib/ET_Client');
 
 describe('requests', function () {
 
+	this.timeout(10000);
+
 	const serverPort = 1234;
+	const serverErrorPort = 4321;
 	const serverMessage = 'Test soap message';
 	const serverResponseEnvelope = `<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><soap:Body><RetrieveResponseMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><Message>${serverMessage}</Message><OverallStatus>OK</OverallStatus></RetrieveResponseMsg></soap:Body></soap:Envelope>`;
 	const server = http.createServer(function (request, response) {
@@ -25,6 +28,17 @@ describe('requests', function () {
             if (err) throw new Error(err);
             assert.equal(response.body.Message, serverMessage);
             assert.equal(response.res.req.path, soapOrigin);
+            done();
+        });
+    });
+
+    it('If origin is changed incorrectly and so the soap request fails, it should return default soap endpoint', done => {
+        const origin = `http://127.0.0.1:${serverErrorPort}/`;
+        const client = new ET_Client(clientId, clientSecret, null, origin, authOrigin, {globalReqOptions});
+        const props = ['ID'];
+        client.clickEvent({props}).get((err, response) => {
+            if (err) throw new Error(err);
+            assert.equal(response.res.request.href, 'https://webservice.exacttarget.com/Service.asmx');
             done();
         });
     });
